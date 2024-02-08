b675a24d8fe878d2615a700eeec458f6ec26cedb
ayasha2001 <c.ayasha.pandey@generalaeronautics.com>
2024-02-08 23:08:50 +0530

    added Link

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,8 +1,26 @@
+import Link from "next/link";
+const details = [
+  { id: 1, name: "Yash", role: "Senior Developer" },
+  { id: 2, name: "Vaibhav", role: "Backend Developer" },
+  { id: 3, name: "Suresh", role: "Frontend Developer" },
+];
 
 const AboutusPage = () => {
   return (
-    <div>this is AboutusPage </div>
-  )
-}
+    <div>
+      <ul>
+        {details.map((item) => {
+          return (
+            <li>
+              <Link href={`/aboutus/${item.id}`} key={item.id}>
+                {item.name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
-export default AboutusPage
+export default AboutusPage;
